2964d846e3db1d3d0e38368b254684688853f9b7
thebrettism <Brettrans@gmail.com>
2021-09-13 09:18:21 +1200

    SCSSPHP Library & include file.

---
+++ includes/compile_scss.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * SCSSPHP
+ *
+ * @copyright 2012-2020 Leaf Corcoran
+ *
+ * @license http://opensource.org/licenses/MIT MIT
+ *
+ * @link http://scssphp.github.io/scssphp
+ */
+
+
+require_once "scss/scss.inc.php";
+
+use ScssPhp\ScssPhp\Compiler;
+
+try {
+
+    $compiled = get_stylesheet_directory().'/style.css';
+    // Check if style.css exists
+    if (file_exists($compiled)) {
+        $startcompile = false;
+        $lastmodified = filemtime($compiled);
+
+        // if the compiled file exists - Get the last modified date
+        // Now Loop through the SCSS asset Directory recursively and look if any monified date
+        // is greater than that one.
+        $di = new RecursiveDirectoryIterator(get_stylesheet_directory().'/assets/scss/');
+        foreach (new RecursiveIteratorIterator($di) as $filename => $file) {
+            // echo $filename . ' - ' . $file->getSize() . ' bytes <br/>';
+            // echo filemtime($filename)."<br/>";
+            if (filemtime($filename) > $lastmodified) {
+                $startcompile = true;
+                break;
+            }
+        }
+    } else {
+        $startcompile = false;
+    }
+    
+    if ($startcompile) {
+
+        $compiler = new Compiler();
+
+        $compiler->setImportPaths(get_stylesheet_directory().'/assets/scss/');
+
+        $compiler->setSourceMap(Compiler::SOURCE_MAP_FILE);
+        $compiler->setSourceMapOptions([
+            // relative or full url to the above .map file
+            'sourceMapURL' => './my-style.map',
+
+            // (optional) relative or full url to the .css file
+            'sourceMapFilename' => 'my-style.css',
+
+            // partial path (server root) removed (normalized) to create a relative url
+            'sourceMapBasepath' => get_stylesheet_directory(),
+
+            // (optional) prepended to 'source' field entries for relocating source files
+            'sourceRoot' => '/',
+        ]);
+
+        $compiler->setOutputStyle('compressed');
+
+        $result = $compiler->compileString('@import "style.scss";');
+
+        file_put_contents(get_stylesheet_directory().'/my-style.map', $result->getSourceMap());
+        file_put_contents(get_stylesheet_directory().'/my-style.css', $result->getCss());
+    }
+
+} catch (\Exception $e) {
+    echo '';
+    syslog(LOG_ERR, 'scssphp: Unable to compile content');
+}
+
+
+
+
+
+
+
+
+
